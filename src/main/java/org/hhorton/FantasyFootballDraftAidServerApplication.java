31798763c9346e7e10cc9602c23427bbd5ece08b
Hunter Horton <hunterhorton89@gmail.com>
2017-06-03 10:49:21 -0500

    initial project commit

---
+++ src/main/java/org/hhorton/FantasyFootballDraftAidServerApplication.java
@@ -0,0 +1,12 @@
+package org.hhorton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyFootballDraftAidServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyFootballDraftAidServerApplication.class, args);
+	}
+}
