2bf0d50483ac926c309cb9b575babc70b30d0956
Hunter Horton <hunterhorton89@gmail.com>
2017-06-03 13:10:12 -0500

    bringing in bootstrap to angular project

--- src/FantasyFootballDraftAidFrontEnd/src/app/app.module.ts
+++ src/FantasyFootballDraftAidFrontEnd/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AlertModule} from "ngx-bootstrap";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
